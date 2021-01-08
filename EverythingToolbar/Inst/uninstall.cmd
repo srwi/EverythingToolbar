87c64926a437b7e9273574f47bd92d02019d0e2d
stnkl <stain.sr@gmail.com>
2021-01-08 18:43:23 +0100

    Better installation process (#68)

---
+++ EverythingToolbar/Inst/uninstall.cmd
@@ -0,0 +1,9 @@
+@echo off
+
+set "params=%*"
+cd /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs" ) && fsutil dirty query %systemdrive% 1>nul 2>nul || (  echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/k cd ""%~sdp0"" && %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs" && "%temp%\getadmin.vbs" && exit /B )
+
+%SystemRoot%\Microsoft.NET\Framework64\v4.0.30319\regasm.exe /nologo /unregister "%~dp0EverythingToolbar.dll"
+taskkill /F /IM explorer.exe & start explorer
+
+pause
--- EverythingToolbar/bin/Release/install.cmd
+++
@@ -1,5 +0,0 @@
-@echo off
-
-%SystemRoot%\Microsoft.NET\Framework64\v4.0.30319\regasm.exe /nologo /codebase "%~dp0EverythingToolbar.dll"
-
-pause
