717ffe834ee5b621c7b5b8e47a540e39b5a9857b
stnkl <stain.sr@gmail.com>
2021-01-09 15:43:33 +0100

    Installation as regular user

--- EverythingToolbar/Inst/install.cmd
+++ EverythingToolbar/Inst/install.cmd
@@ -1,8 +1,29 @@
 @echo off
 
-set "params=%*"
-cd /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs" ) && fsutil dirty query %systemdrive% 1>nul 2>nul || (  echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/k cd ""%~sdp0"" && %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs" && "%temp%\getadmin.vbs" && exit /B )
+echo Installation directory:
 
-%SystemRoot%\Microsoft.NET\Framework64\v4.0.30319\regasm.exe /nologo /codebase "%~dp0EverythingToolbar.dll"
+set dll_path=%~dp0
+echo %dll_path%
+
+echo Registering EverythingToolbar...
+
+set base_key="HKEY_CURRENT_USER\Software\Classes\CLSID\{9D39B79C-E03C-4757-B1B6-ECCE843748F3}"
+reg.exe add "%base_key%" /ve /t "REG_SZ" /d "Everything Toolbar" /f
+reg.exe add "%base_key%\Implemented Categories" /f
+reg.exe add "%base_key%\Implemented Categories\{00021492-0000-0000-c000-000000000046}" /f
+reg.exe add "%base_key%\Implemented Categories\{62C8FE65-4EBB-45e7-B440-6E39B2CDBF29} /f{62C8FE65-4EBB-45e7-B440-6E39B2CDBF29}" /f
+reg.exe add "%base_key%\InprocServer32" /ve /t "REG_SZ" /d "mscoree.dll" /f
+reg.exe add "%base_key%\InprocServer32" /v "ThreadingModel" /t "REG_SZ" /d "Both" /f
+reg.exe add "%base_key%\InprocServer32" /v "Class" /t "REG_SZ" /d "CSDeskBand.Deskband" /f
+reg.exe add "%base_key%\InprocServer32" /v "Assembly" /t "REG_SZ" /d "EverythingToolbar, Version=0.6.1.0, Culture=neutral, PublicKeyToken=3b7d0980372ca18d" /f
+reg.exe add "%base_key%\InprocServer32" /v "RuntimeVersion" /t "REG_SZ" /d "v4.0.30319" /f
+reg.exe add "%base_key%\InprocServer32" /v "CodeBase" /t "REG_SZ" /d "file:///%dll_path%EverythingToolbar.dll" /f
+reg.exe add "%base_key%\InprocServer32\0.6.1.0" /v "Class" /t "REG_SZ" /d "CSDeskBand.Deskband" /f
+reg.exe add "%base_key%\InprocServer32\0.6.1.0" /v "Assembly" /t "REG_SZ" /d "EverythingToolbar, Version=0.6.1.0, Culture=neutral, PublicKeyToken=3b7d0980372ca18d" /f
+reg.exe add "%base_key%\InprocServer32\0.6.1.0" /v "RuntimeVersion" /t "REG_SZ" /d "v4.0.30319" /f
+reg.exe add "%base_key%\InprocServer32\0.6.1.0" /v "CodeBase" /t "REG_SZ" /d "file:///%dll_path%EverythingToolbar.dll" /f
+reg.exe add "%base_key%\ProgId" /ve /t "REG_SZ" /d "CSDeskBand.Deskband" /f
+
+echo Please open the taskbar context menu twice for EverythingToolbar to show up!
 
 pause
--- EverythingToolbar/Properties/AssemblyInfo.cs
+++ EverythingToolbar/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("EverythingToolbar")]
-[assembly: AssemblyCopyright("Copyright © Stephan Rumswinkel 2020")]
+[assembly: AssemblyCopyright("Copyright © Stephan Rumswinkel 2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
