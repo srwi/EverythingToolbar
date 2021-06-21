067d17da69894b5e33970d6c739f6b643e45e7c1
Stephan Rumswinkel <stnkl@users.noreply.github.com>
2021-06-21 11:32:45 +0200

    Use ElementHost in Winforms to fix #4 and #15

---
+++ EverythingToolbar.Deskband/Deskband.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace EverythingToolbar.Deskband
+{
+    [ComVisible(true)]
+    [Guid("FB17B6DA-E3D7-4D17-9E43-3416988372A9")]
+    [CSDeskBand.CSDeskBandRegistration(Name = "Everything Toolbar WinForms", ShowDeskBand = false)]
+    public class Deskband : CSDeskBand.CSDeskBandWin
+    {
+        private static Control control;
+
+        public Deskband()
+        {
+            Options.MinHorizontalSize = new Size(18, 30);
+            control = new ToolbarControlHost(this);
+        }
+
+        protected override Control Control => control;
+    }
+}
---
+++ EverythingToolbar.Deskband/ToolbarControlHost.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows.Forms;
+using System.Windows.Forms.Integration;
+
+namespace EverythingToolbar.Deskband
+{
+    public partial class ToolbarControlHost : UserControl
+    {
+        public ToolbarControlHost(CSDeskBand.CSDeskBandWin w)
+        {
+            InitializeComponent();
+            //this.BackColor = Color.LimeGreen;
+            //this.TransparencyKey = Color.LimeGreen;
+            ElementHost host = new ElementHost
+            {
+                Dock = DockStyle.Fill,
+                BackColorTransparent = true,
+                Child = new ToolbarControl()
+            };
+            Controls.Add(host);
+        }
+
+        protected override void OnPaintBackground(PaintEventArgs e) { }
+    }
+}
