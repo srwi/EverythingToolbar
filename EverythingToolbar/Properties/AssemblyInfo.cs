7811f8091dc0f574f354baec18eb2d7aea11065e
stnkl <stnkl@users.noreply.github.com>
2021-06-29 20:08:12 +0200

    Bump version

--- EverythingToolbar/Properties/AssemblyInfo.cs
+++ EverythingToolbar/Properties/AssemblyInfo.cs
@@ -49,5 +49,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.7.0.1")]
-[assembly: AssemblyFileVersion("0.7.0.1")]
+[assembly: AssemblyVersion("0.7.0.2")]
+[assembly: AssemblyFileVersion("0.7.0.2")]
