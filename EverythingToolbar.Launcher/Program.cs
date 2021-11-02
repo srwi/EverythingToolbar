b4dd4b62a68174e17828bc9fc0ca5862769ac49e
stnkl <stnkl@users.noreply.github.com>
2021-11-02 19:38:59 +0100

    Add launcher project

---
+++ EverythingToolbar.Launcher/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Windows;
+
+namespace EverythingToolbar.Launcher
+{
+    class Program
+    {
+        public partial class LauncherWindow : Window
+        {
+            public LauncherWindow()
+            {
+                Title = "EverythingToolbar";
+                Width = 0;
+                Height = 0;
+                Opacity = 0;
+                ResizeMode = ResizeMode.NoResize;
+                WindowStyle = WindowStyle.None;
+                Topmost = true;
+                SearchResultsPopup.taskbarEdge = CSDeskBand.Edge.Bottom;
+                ToolbarControl tc = new ToolbarControl();
+                Content = tc;
+                EverythingSearch.Instance.SearchTerm = "";
+                SearchResultsPopup searchResultsPopup = (SearchResultsPopup)tc.FindName("SearchResultsPopup");
+                searchResultsPopup.Closed += OnPopupClosed;
+            }
+
+            private void OnPopupClosed(object sender, EventArgs e)
+            {
+                Close();
+            }
+        }
+
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Application app = new Application();
+            app.Run(new LauncherWindow());
+        }
+    }
+}
