e1e77a8911b144b01e9e243754cfa3239089fb60
stnkl <stnkl@users.noreply.github.com>
2021-11-06 00:04:12 +0100

    Open in middle of taskbar

--- EverythingToolbar.Launcher/Program.cs
+++ EverythingToolbar.Launcher/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
 using System.Windows;
+using System.Windows.Forms;
 
 namespace EverythingToolbar.Launcher
 {
@@ -9,9 +12,14 @@ namespace EverythingToolbar.Launcher
         {
             public LauncherWindow()
             {
+                List<Rectangle> taskbars = FindDockedTaskBars();
+                Rectangle taskbar = taskbars[0];
                 Title = "EverythingToolbar";
                 Width = 0;
                 Height = 0;
+                Left = taskbar.Width / 2;
+                Top = taskbar.Y;
+                
                 Opacity = 0;
                 ResizeMode = ResizeMode.NoResize;
                 WindowStyle = WindowStyle.None;
@@ -19,21 +27,71 @@ namespace EverythingToolbar.Launcher
                 SearchResultsPopup.taskbarEdge = CSDeskBand.Edge.Bottom;
                 ToolbarControl tc = new ToolbarControl();
                 Content = tc;
-                EverythingSearch.Instance.SearchTerm = "";
                 SearchResultsPopup searchResultsPopup = (SearchResultsPopup)tc.FindName("SearchResultsPopup");
                 searchResultsPopup.Closed += OnPopupClosed;
+                EverythingSearch.Instance.SearchTerm = "";
             }
 
             private void OnPopupClosed(object sender, EventArgs e)
             {
                 Close();
             }
+
+            public static List<Rectangle> FindDockedTaskBars()
+            {
+                List<Rectangle> dockedRects = new List<Rectangle>();
+                foreach (var tmpScrn in Screen.AllScreens)
+                {
+                    if (!tmpScrn.Bounds.Equals(tmpScrn.WorkingArea))
+                    {
+                        Rectangle rect = new Rectangle();
+
+                        var leftDockedWidth = Math.Abs((Math.Abs(tmpScrn.Bounds.Left) - Math.Abs(tmpScrn.WorkingArea.Left)));
+                        var topDockedHeight = Math.Abs((Math.Abs(tmpScrn.Bounds.Top) - Math.Abs(tmpScrn.WorkingArea.Top)));
+                        var rightDockedWidth = ((tmpScrn.Bounds.Width - leftDockedWidth) - tmpScrn.WorkingArea.Width);
+                        var bottomDockedHeight = ((tmpScrn.Bounds.Height - topDockedHeight) - tmpScrn.WorkingArea.Height);
+
+                        if ((leftDockedWidth > 0))
+                        {
+                            rect.X = tmpScrn.Bounds.Left;
+                            rect.Y = tmpScrn.Bounds.Top;
+                            rect.Width = leftDockedWidth;
+                            rect.Height = tmpScrn.Bounds.Height;
+                        }
+                        else if ((rightDockedWidth > 0))
+                        {
+                            rect.X = tmpScrn.WorkingArea.Right;
+                            rect.Y = tmpScrn.Bounds.Top;
+                            rect.Width = rightDockedWidth;
+                            rect.Height = tmpScrn.Bounds.Height;
+                        }
+                        else if ((topDockedHeight > 0))
+                        {
+                            rect.X = tmpScrn.WorkingArea.Left;
+                            rect.Y = tmpScrn.Bounds.Top;
+                            rect.Width = tmpScrn.WorkingArea.Width;
+                            rect.Height = topDockedHeight;
+                        }
+                        else if ((bottomDockedHeight > 0))
+                        {
+                            rect.X = tmpScrn.WorkingArea.Left;
+                            rect.Y = tmpScrn.WorkingArea.Bottom;
+                            rect.Width = tmpScrn.WorkingArea.Width;
+                            rect.Height = bottomDockedHeight;
+                        }
+
+                        dockedRects.Add(rect);
+                    }
+                }
+
+                return dockedRects;
+            }
         }
 
         [STAThread]
         static void Main(string[] args)
         {
-            Application app = new Application();
+            System.Windows.Application app = new System.Windows.Application();
             app.Run(new LauncherWindow());
         }
     }
