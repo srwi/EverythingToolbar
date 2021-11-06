156f28b933bde13d9a3afa79d232b3a9c8c8c47e
stnkl <stnkl@users.noreply.github.com>
2021-11-06 23:57:54 +0100

    Set popup position

--- EverythingToolbar/Properties/Settings.Designer.cs
+++ EverythingToolbar/Properties/Settings.Designer.cs
@@ -13,7 +13,7 @@ namespace EverythingToolbar.Properties {
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.10.0.0")]
-    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
+    public sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
         
--- EverythingToolbar/Properties/Settings.cs
+++ EverythingToolbar/Properties/Settings.cs
@@ -6,7 +6,7 @@
     //  The PropertyChanged event is raised after a setting's value is changed.
     //  The SettingsLoaded event is raised after the setting values are loaded.
     //  The SettingsSaving event is raised before the setting values are saved.
-    internal sealed partial class Settings {
+    public sealed partial class Settings {
         
         public Settings() {
             // // To add event handlers for saving and changing settings, uncomment the lines below:
