92319dc0592cff75a18341e766c05c2b4625e9e6
stnkl <stnkl@users.noreply.github.com>
2022-01-20 23:07:55 +0100

    Bump version

--- EverythingToolbar.Debug/Properties/AssemblyInfo.cs
+++ EverythingToolbar.Debug/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("EverythingToolbar.Debug")]
-[assembly: AssemblyCopyright("Copyright © Stephan Rumswinkel 2021")]
+[assembly: AssemblyCopyright("Copyright © Stephan Rumswinkel 2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
