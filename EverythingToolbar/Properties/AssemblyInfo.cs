c5b7aa701dd74ee759594a9589d9f7d0930a5789
stnkl <stnkl@users.noreply.github.com>
2022-01-20 23:07:55 +0100

    Bump version

--- EverythingToolbar/Properties/AssemblyInfo.cs
+++ EverythingToolbar/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("EverythingToolbar")]
-[assembly: AssemblyCopyright("Copyright © Stephan Rumswinkel 2021")]
+[assembly: AssemblyCopyright("Copyright © Stephan Rumswinkel 2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -49,5 +49,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.7.3.0")]
-[assembly: AssemblyFileVersion("0.7.3.0")]
+[assembly: AssemblyVersion("0.7.4.0")]
+[assembly: AssemblyFileVersion("0.7.4.0")]
