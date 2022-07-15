e785f1d8ffe8a98cd32fcad07fc3bafaeb1d8cc0
stnkl <stnkl@users.noreply.github.com>
2022-07-15 19:13:09 +0200

    Bump version

--- EverythingToolbar.Launcher/Properties/AssemblyInfo.cs
+++ EverythingToolbar.Launcher/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("EverythingToolbar.Launcher")]
-[assembly: AssemblyCopyright("Copyright © Stephan Rumswinkel 2021")]
+[assembly: AssemblyCopyright("Copyright © Stephan Rumswinkel 2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
