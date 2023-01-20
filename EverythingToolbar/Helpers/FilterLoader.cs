98f5fbc137a96ff75bd4a6b96b8bd5b5829b50d4
stnkl <stnkl@users.noreply.github.com>
2023-01-20 20:43:49 +0100

    Remove debug project

--- EverythingToolbar/Helpers/FilterLoader.cs
+++ EverythingToolbar/Helpers/FilterLoader.cs
@@ -4,7 +4,6 @@ using NLog;
 using System;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
-using System.Drawing.Text;
 using System.IO;
 using System.Linq;
 using System.Runtime.CompilerServices;
