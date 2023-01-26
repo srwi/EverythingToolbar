fea28a684ae6031c862a1507caec0528279b9d05
stnkl <stnkl@users.noreply.github.com>
2023-01-26 21:27:03 +0100

    Update clean solution script

--- tools/clean_solution.cmd
+++ tools/clean_solution.cmd
@@ -1,5 +1,7 @@
 @echo off
 
+taskkill /f /im explorer.exe 2>nul &set errorlevel=0
+
 rd /s /q "%~dp0..\EverythingSDK\x64"
 rd /s /q "%~dp0..\EverythingToolbar\bin"
 rd /s /q "%~dp0..\EverythingToolbar\obj"
@@ -10,5 +12,7 @@ rd /s /q "%~dp0..\EverythingToolbar.Launcher\obj"
 rd /s /q "%~dp0..\EverythingToolbar.Installer\bin"
 rd /s /q "%~dp0..\EverythingToolbar.Installer\obj"
 del /f "%~dp0..\EverythingToolbar.Installer\HeatGeneratedFileList.wxs"
+
+powershell start-process %windir%\explorer.exe
 
 pause
