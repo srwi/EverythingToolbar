83e255a58d539f120b99dd505a59d9683ec19bfe
stnkl <17520641+srwi@users.noreply.github.com>
2023-04-01 00:49:30 +0200

    Revamp setup assistant

--- EverythingToolbar.Launcher/SetupAssistant.xaml.cs
+++ EverythingToolbar.Launcher/SetupAssistant.xaml.cs
@@ -13,6 +13,8 @@ namespace EverythingToolbar.Launcher
     {
         private readonly string _taskbarShortcutPath = Utils.GetTaskbarShortcutPath();
         private readonly NotifyIcon _icon;
+        private const int TotalPages = 3;
+        private int _unlockedPages = 1;
         private bool _iconHasChanged;
         private FileSystemWatcher _watcher;
 
@@ -24,8 +26,13 @@ namespace EverythingToolbar.Launcher
 
             AutostartCheckBox.IsChecked = Utils.GetAutostartState();
             HideWindowsSearchCheckBox.IsChecked = !Utils.GetWindowsSearchEnabledState();
-            UpdateSetupStepVisibility();
             CreateFileWatcher();
+            
+            if (File.Exists(_taskbarShortcutPath))
+            {
+                _unlockedPages = Math.Max(3, _unlockedPages);
+                Dispatcher.Invoke(() => { SelectPage(2); });
+            }
 
             Loaded += OnLoaded;
         }
@@ -53,46 +60,23 @@ namespace EverythingToolbar.Launcher
                 EnableRaisingEvents = true
             };
 
-            _watcher.Created += (source, e) => {
-                _iconHasChanged = true;
-                UpdateSetupStepVisibility();
-            };
-            _watcher.Deleted += (source, e) => {
-                UpdateSetupStepVisibility();
-            };
-        }
-
-        private void UpdateSetupStepVisibility()
-        {
-            Dispatcher.Invoke(() =>
+            _watcher.Created += (source, e) =>
             {
-                if (!string.IsNullOrEmpty(Settings.Default.iconName))
-                {
-                    SectionTwo.Opacity = 1.0;
-                    SectionTwo.IsHitTestVisible = true;
-                }
-                else
-                {
-                    SectionTwo.Opacity = 0.2;
-                    SectionTwo.IsHitTestVisible = false;
-                }
-
-                if (File.Exists(_taskbarShortcutPath))
-                {
-                    SectionThree.Opacity = 1.0;
-                    SectionThree.IsHitTestVisible = true;
-                }
-                else
-                {
-                    SectionThree.Opacity = 0.2;
-                    SectionThree.IsHitTestVisible = false;
-                }
-            });
+                _iconHasChanged = true;
+                _unlockedPages = Math.Max(3, _unlockedPages);
+                Dispatcher.Invoke(() => { SelectPage(2); });
+            };
+            _watcher.Deleted += (source, e) =>
+            {
+                _unlockedPages = Math.Min(2, _unlockedPages);
+                Dispatcher.Invoke(() => { SelectPage(1); });
+            };
         }
 
         private void HideWindowsSearchChanged(object sender, RoutedEventArgs e)
         {
-            Utils.SetWindowsSearchEnabledState(HideWindowsSearchCheckBox.IsChecked != null && !(bool)HideWindowsSearchCheckBox.IsChecked);
+            Utils.SetWindowsSearchEnabledState(HideWindowsSearchCheckBox.IsChecked != null &&
+                                               !(bool)HideWindowsSearchCheckBox.IsChecked);
         }
 
         private void AutostartChanged(object sender, RoutedEventArgs e)
@@ -119,7 +103,8 @@ namespace EverythingToolbar.Launcher
                 return;
 
             if (MessageBox.Show(Properties.Resources.SetupAssistantRestartExplorerDialogText,
-                Properties.Resources.SetupAssistantRestartExplorerDialogTitle, MessageBoxButton.YesNo) == MessageBoxResult.Yes)
+                    Properties.Resources.SetupAssistantRestartExplorerDialogTitle, MessageBoxButton.YesNo) ==
+                MessageBoxResult.Yes)
             {
                 Utils.ChangeTaskbarPinIcon(Settings.Default.iconName);
             }
@@ -130,7 +115,34 @@ namespace EverythingToolbar.Launcher
             Settings.Default.iconName = ((RadioButton)sender).Tag as string;
             Icon = new BitmapImage(new Uri("pack://application:,,,/" + Settings.Default.iconName));
             _iconHasChanged = true;
-            UpdateSetupStepVisibility();
+
+            _unlockedPages = Math.Max(2, _unlockedPages);
+            UpdatePagination();
+        }
+
+        private void SelectPage(int page)
+        {
+            PaginationTabControl.SelectedIndex = page;
+            UpdatePagination();
+        }
+
+        private void UpdatePagination()
+        {
+            PreviousButton.IsEnabled = PaginationTabControl.SelectedIndex > 0;
+            NextButton.IsEnabled = PaginationTabControl.SelectedIndex < _unlockedPages - 1;
+            PaginationLabel.Text = $"{PaginationTabControl.SelectedIndex + 1} / {TotalPages}";
+        }
+
+        private void OnNextPageClicked(object sender, RoutedEventArgs e)
+        {
+            var nextPage = Math.Min(PaginationTabControl.SelectedIndex + 1, _unlockedPages - 1);
+            SelectPage(nextPage);
+        }
+
+        private void OnPreviousPageClicked(object sender, RoutedEventArgs e)
+        {
+            var previousPage = Math.Max(PaginationTabControl.SelectedIndex - 1, 0);
+            SelectPage(previousPage);
         }
     }
 }
