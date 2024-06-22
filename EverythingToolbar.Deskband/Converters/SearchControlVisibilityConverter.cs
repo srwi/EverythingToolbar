fb596b14a51d4a5cc7fa7eb8e0f78b220a19bad5
Stephan Rumswinkel <17520641+srwi@users.noreply.github.com>
2024-06-22 22:40:50 +0200

    Force search button in deskband when auto-hiding taskbar is enabled (#4)
    Closes #4

---
+++ EverythingToolbar.Deskband/Converters/SearchControlVisibilityConverter.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Globalization;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Markup;
+using CSDeskBand.Interop;
+using EverythingToolbar.Helpers;
+
+namespace EverythingToolbar.Deskband.Converters
+{
+    public class SearchControlVisibilityConverter : MarkupExtension, IValueConverter
+    {
+        public bool AlwaysVisibleWithAutoHidingTaskbar  { get; set; }
+        public double VisibilityThreshold { get; set; }
+
+        private static bool _isTaskbarAutoHiding;
+
+        public SearchControlVisibilityConverter()
+        {
+            // We get the taskbar auto hide state only once for now as it is not expected to change often
+            SetTaskbarAutoHideState();
+        }
+
+        private void SetTaskbarAutoHideState()
+        {
+            const uint ABS_AUTOHIDE = 0x0000001;
+            var autoHideData = new APPBARDATA
+            {
+                hWnd = IntPtr.Zero,
+                cbSize = Marshal.SizeOf<APPBARDATA>()
+            };
+            var autoHideState = Shell32.SHAppBarMessage(APPBARMESSAGE.ABM_GETSTATE, ref autoHideData);
+            if (autoHideState != IntPtr.Zero)
+            {
+                _isTaskbarAutoHiding = ((int)autoHideState.ToInt64() & ABS_AUTOHIDE) == ABS_AUTOHIDE;
+                ToolbarLogger.GetLogger("").Info(_isTaskbarAutoHiding.ToString());
+            }
+        }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (_isTaskbarAutoHiding)
+                return AlwaysVisibleWithAutoHidingTaskbar ? Visibility.Visible : Visibility.Collapsed;
+
+            if (System.Convert.ToDouble(value) >= Math.Abs(VisibilityThreshold))
+            {
+                return VisibilityThreshold >= 0 ? Visibility.Visible : Visibility.Hidden;
+            }
+
+            return VisibilityThreshold >= 0 ? Visibility.Hidden : Visibility.Visible;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException("This converter cannot be used in two-way binding.");
+        }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return this;
+        }
+    }
+}
