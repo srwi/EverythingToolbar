664009ca09839478b92c7c91da569742d89b471d
Stephan Rumswinkel <17520641+srwi@users.noreply.github.com>
2024-09-07 23:09:07 +0200

    Fix deskband logging regression

--- EverythingToolbar/Helpers/ToolbarLogger.cs
+++ EverythingToolbar/Helpers/ToolbarLogger.cs
@@ -47,10 +47,15 @@ namespace EverythingToolbar.Helpers
             {
                 logger.Error((Exception)args.ExceptionObject, "Unhandled exception");
             };
-            Application.Current.DispatcherUnhandledException += (sender, args) =>
+
+            if (Application.Current != null)
             {
-                logger.Error(args.Exception, "Unhandled exception on UI thread");
-            };
+                // Not applicable for deskband
+                Application.Current.DispatcherUnhandledException += (sender, args) =>
+                {
+                    logger.Error(args.Exception, "Unhandled exception on UI thread");
+                };
+            }
         }
 
         private static void ConfigureLogger()
