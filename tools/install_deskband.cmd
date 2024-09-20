af9887c336695b34f18720c33d97dfffdf45f3e5
Stephan Rumswinkel <17520641+srwi@users.noreply.github.com>
2024-09-20 19:20:05 +0200

    Override previous deskband registration

--- tools/install_deskband.cmd
+++ tools/install_deskband.cmd
@@ -1,5 +1,6 @@
 @echo off
 
+%SystemRoot%\Microsoft.NET\Framework64\v4.0.30319\regasm.exe /nologo /unregister "%~dp0..\EverythingToolbar.Deskband\bin\Release\EverythingToolbar.Deskband.dll"
 %SystemRoot%\Microsoft.NET\Framework64\v4.0.30319\regasm.exe /nologo /codebase "%~dp0..\EverythingToolbar.Deskband\bin\Release\EverythingToolbar.Deskband.dll"
 
 pause
