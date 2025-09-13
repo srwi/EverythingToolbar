c3f1da10760266e4750d207a74004a5064b1ecda
DoLovya <3063676257@qq.com>
2025-09-13 16:26:47 +0800

    Error occurred while running the 'XXX.cmd' scripts (#664)

--- tools/install_deskband.cmd
+++ tools/install_deskband.cmd
@@ -1,9 +1,9 @@
 @echo off
 setlocal enabledelayedexpansion
 
-set "PATH=%~dp0..\EverythingToolbar.Deskband\bin\x64\Release\net8.0-windows10.0.17763.0\EverythingToolbar.Deskband.comhost.dll"
+set "COM_DLL_PATH=%~dp0..\EverythingToolbar.Deskband\bin\x64\Release\net8.0-windows10.0.17763.0\EverythingToolbar.Deskband.comhost.dll"
 
-regsvr32 "%PATH%"
+regsvr32 "%COM_DLL_PATH%"
 
 taskkill /f /im explorer.exe >nul 2>&1
 timeout /t 1 >nul
