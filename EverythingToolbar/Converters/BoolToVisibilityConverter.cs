ca2d430608c525a18d60d5438c65c2393d26e6c4
Stephan Rumswinkel <17520641+srwi@users.noreply.github.com>
2026-07-16 19:02:33 +0200

    Use markup extensions for bool-to-visibility converters

---
+++ EverythingToolbar/Converters/BoolToVisibilityConverter.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace EverythingToolbar.Converters
+{
+    public class BoolToVisibilityConverter : MarkupExtension, IValueConverter
+    {
+        public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            if (value is bool boolean && boolean)
+                return Visibility.Visible;
+
+            return Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException("This converter cannot be used in two-way binding.");
+        }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return this;
+        }
+    }
+}
